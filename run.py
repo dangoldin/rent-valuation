aa678b2a8c13418b61a3cd850f03924321c85736
Dan Goldin <dgoldin@triplelift.com>
2017-09-03 21:27:54 -0400

    Clean up some code

--- run.py
+++ run.py
@@ -3,6 +3,7 @@
 import sys
 import requests
 import config
+import re
 
 import xml.etree.ElementTree
 import xml.dom.minidom
@@ -12,6 +13,21 @@ BASE_URL = 'http://www.zillow.com/webservice'
 GET_SEARCH_RESULTS = BASE_URL + '/GetSearchResults.htm'
 GET_ESTIMATE = BASE_URL + '/GetZestimate.htm'
 GET_PROPERTY_DETAILS = BASE_URL + '/GetUpdatedPropertyDetails.htm'
+GET_DEEP_COMPS = BASE_URL + '/GetDeepComps.htm'
+
+RE_XML_ERROR_CODE = re.compile(r'<code>(\d+)</code>')
+
+def pretty_print_xml(xml_string):
+    return xml.dom.minidom.parseString(xml_string).toprettyxml()
+
+def get_error_code(xml_string):
+    error_codes = RE_XML_ERROR_CODE.findall(xml_string)
+    if len(error_codes):
+        return int(error_codes[0])
+    return 0
+
+def is_valid_request(r):
+    return not (r.status_code >= 300 or get_error_code(r.content) > 0)
 
 def get_top_match(addr, zipc):
     r = requests.get(GET_SEARCH_RESULTS, params={
@@ -19,7 +35,9 @@ def get_top_match(addr, zipc):
         'address': addr,
         'citystatezip': zipc,
     })
-    # TODO: Handle failure case
+    if not is_valid_request(r):
+        print 'Failed to find address', addr, zipc
+        return None
 
     # Return the first one
     e = xml.etree.ElementTree.fromstring(r.content)
@@ -33,23 +51,36 @@ def get_estimate(zpid):
         'zpid': zpid,
         'rentzestimate': 'true',
     })
-    # TODO: Handle failure case
+    if not is_valid_request(r):
+        print 'Failed to get estimate for', zpid
+        return None
 
-    print r.content
     e = xml.etree.ElementTree.fromstring(r.content)
-
-    xml2 = xml.dom.minidom.parseString(r.content)
-    pretty_xml_as_string = xml2.toprettyxml()
-    print pretty_xml_as_string
+    print pretty_print_xml(r.content)
 
 def get_property_details(zpid):
     r = requests.get(GET_PROPERTY_DETAILS, params={
         'zws-id': config.ZWSID,
-        'zpid': zpid
+        'zpid': zpid,
     })
-    # TODO: Handle failure case
+    if not is_valid_request(r):
+        print 'Failed to get property details for', zpid
+        return None
 
-    print r.content
+    print pretty_print_xml(r.content)
+
+def get_deep_comps(zpid):
+    r = requests.get(GET_DEEP_COMPS, params={
+        'zws-id': config.ZWSID,
+        'zpid': zpid,
+        'count': 25,
+        'rentzestimate': 'true',
+    })
+    if not is_valid_request(r):
+        print 'Failed to get deep comps for', zpid
+        return None
+
+    print pretty_print_xml(r.content)
 
 if __name__ == '__main__':
     ADDRESS = sys.argv[1]
@@ -60,3 +91,4 @@ if __name__ == '__main__':
     print 'Received ZPID', zpid
     get_estimate(zpid)
     get_property_details(zpid)
+    get_deep_comps(zpid)
